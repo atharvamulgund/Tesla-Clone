e96ce908a0381779335fc79ea1180e9bc30699a5
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-06-13 18:51:34 +0530

    Fixed the rendering issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,46 @@
 import React from 'react'
-import Section from './Section'
+import Section from './Section.js'
 
 const Home = () => {
   return (
-    <section className="home-container height-screen ">
-        <Section></Section>
-        {/* <Section></Section>
-        <Section></Section>
-        <Section></Section> */}
-        </section>
+    <div className="home-container height-screen ">
+        <Section 
+        title="Model-s"
+        description="Order Online for Touchless Delivery"
+        image="../images/model-s.jpg"
+        alt="model-s"
+        />
+        <Section 
+        title="Model-x"
+        description="Order Online for Touchless Delivery"
+        image="../images/model-x.jpg"
+        alt="model-x"
+        />
+        <Section 
+        title="Model-y"
+        description="Order Online for Touchless Delivery"
+        image="../images/model-y.jpg"
+        alt="model-y"
+        />
+        <Section 
+        title="Model-3"
+        description="Order Online for Touchless Delivery"
+        image="../images/model-3.jpg"
+        alt="model-3"
+        />
+        <Section 
+        title="Solar Panels"
+        description="Order Online for Touchless Delivery"
+        image="../images/solar-panel.jpg"
+        alt="solar-panel"
+        />
+        <Section 
+        title="Solar Roof"
+        description="Order Online for Touchless Delivery"
+        image="../images/solar-roof.jpg"
+        alt="Solar Roof"
+        />
+    </div>
   )
 }
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,34 +1,26 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './section.css'
-import Highlight from './carHighlight'
-const Section = () => {
-       
-            const [Carinfo, setCarInfo] = useState(Highlight)
-                Highlight.map((index)=>{
-                    const {id,title,description,image} = index
-                    return(
-                        <section className="section-container w-screen h-screen relative">
-                            <img src="../images/model-s.jpg" alt="Model-s" className='h-full w-full' />
-                             <div className="item-text absolute mx-auto top-0 left-0 right-0 flex flex-col justify-center items-center mt-20 text-center" >
-                                 <h1 className="Heading">
-                                    {title}
-                                 </h1>
-                                <p className='sub-heading w-full'>{description}</p>
-                             </div>
-                             <div className="btn-group absolute bottom-0 left-0 right-0 flex justify-center items-center  mb-20 gap-5 uppercase ">
-                                <div className="left-btn">
-                                    <button>Custom Order</button>
-                                 </div>
-                                 <div className="right-btn">Exisiting Inventory</div>
-                                </div>
-                             <div className="down-arrow absolute bottom-0 left-0 right-0 flex justify-center items-center flex-col  gap-5">
-                                 <img src="../images/down-arrow.svg" alt="Arrow" className='w-10 h-10'/>
-                             </div>
-                        </section>  
-                    )
-                })
-            
-        
+const Section = (props) => {
+    return(
+        <section className="section-container w-screen h-screen relative">
+        <img src={props.image} alt={props.alt} className='h-full w-full' />
+        <div className="item-text absolute mx-auto top-0 left-0 right-0 flex flex-col justify-center items-center mt-20 text-center" >
+            <h1 className="Heading">
+                {props.title}
+            </h1>
+            <p className='sub-heading w-full'>{props.description}</p>
+        </div>
+        <div className="btn-group absolute bottom-0 left-0 right-0 flex justify-center items-center  mb-20 gap-5 uppercase ">
+            <div className="left-btn">
+                <button>Custom Order</button>
+            </div>
+            <div className="right-btn">Exisiting Inventory</div>
+        </div>
+            <div className="down-arrow absolute bottom-0 left-0 right-0 flex justify-center items-center flex-col  gap-5">
+                <img src="../images/down-arrow.svg" alt="Arrow" className='w-10 h-10'/>
+            </div>
+</section> 
+    )
 }
 
 export default Section
@@ -56,3 +48,29 @@ export default Section
                 <img src="../images/down-arrow.svg" alt="Arrow" className='w-10 h-10'/>
             </div>
 </section> */}
+
+// const [Carinfo, setCarInfo] = useState(Highlight)
+//                 Highlight.map((index)=>{
+//                     const {id,title,description,image} = index
+//                     return(
+//                         <section className="section-container w-screen h-screen relative">
+//                             <img src="../images/model-s.jpg" alt="Model-s" className='h-full w-full' />
+//                              <div className="item-text absolute mx-auto top-0 left-0 right-0 flex flex-col justify-center items-center mt-20 text-center" >
+//                                  <h1 className="Heading">
+//                                     {title}
+//                                  </h1>
+//                                 <p className='sub-heading w-full'>{description}</p>
+//                              </div>
+//                              <div className="btn-group absolute bottom-0 left-0 right-0 flex justify-center items-center  mb-20 gap-5 uppercase ">
+//                                 <div className="left-btn">
+//                                     <button>Custom Order</button>
+//                                  </div>
+//                                  <div className="right-btn">Exisiting Inventory</div>
+//                                 </div>
+//                              <div className="down-arrow absolute bottom-0 left-0 right-0 flex justify-center items-center flex-col  gap-5">
+//                                  <img src="../images/down-arrow.svg" alt="Arrow" className='w-10 h-10'/>
+//                              </div>
+//                         </section>  
+//                     )
+//                 })
+            
